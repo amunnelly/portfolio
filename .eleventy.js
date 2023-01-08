e62a65f4e19bceb06a0e50a71bd793608cf32da9
amunnelly <anthony.munnelly@gmail.com>
2023-01-08 00:05:44 +0000

    moved favicon to fav directory, added passthrough copy directive to .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -38,7 +38,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addWatchTarget("./src/css");
 
-    eleventyConfig.addPassthroughCopy({ ".src/images/favicon": "/" });
+    eleventyConfig.addPassthroughCopy("./src/fav");
     
     eleventyConfig.addPlugin(syntaxHighlight);
 
