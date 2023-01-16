3aa77c2553c569b1ba6909bbdb285351d7c4cb14
amunnelly <anthony.munnelly@gmail.com>
2023-01-16 19:48:36 +0000

    fixed number of homepage links issue

--- .eleventy.js
+++ .eleventy.js
@@ -37,7 +37,6 @@ module.exports = function (eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addWatchTarget("./src/css");
-
     eleventyConfig.addPassthroughCopy("./src/fav");
     
     eleventyConfig.addPlugin(syntaxHighlight);
